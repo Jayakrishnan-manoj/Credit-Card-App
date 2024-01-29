d84f83306a483ba8ad497f179da6a77def944915
jk.jayakrishnan27 <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2024-01-29 23:18:12 +0530

    complete card UI

--- app/src/main/java/com/example/creditcard_app/creditCard.kt
+++ app/src/main/java/com/example/creditcard_app/creditCard.kt
@@ -1,16 +1,23 @@
 package com.example.creditcard_app
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CreditCard(cardInfo: CardInfo) {
@@ -26,5 +33,20 @@ fun CreditCard(cardInfo: CardInfo) {
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.FillBounds,
         )
+
+        Box(modifier = Modifier.padding(16.dp)) {
+            Image(
+                painter = painterResource(id = cardInfo.providerImage),
+                contentDescription = "Provider image",
+                modifier = Modifier
+                    .width(86.dp)
+                    .align(Alignment.TopStart)
+            )
+            Column(modifier = Modifier.align(Alignment.BottomStart)) {
+
+                Text(text = cardInfo.cardNumber, fontSize = 16.sp, letterSpacing = 1.2.sp)
+                Text(text = cardInfo.cardHolder, fontSize = 16.sp, letterSpacing = 1.2.sp)
+            }
+        }
     }
 }
